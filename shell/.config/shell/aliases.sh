b51a26fa2d313e6392acddb827953b76e6cb7927
fiorava <constfiora@gmail.com>
2022-11-26 18:59:46 +0200

    -

---
+++ shell/.config/shell/aliases.sh
@@ -0,0 +1,17 @@
+alias\
+	chgrp='chgrp --preserve-root'\
+	chmod='chmod --preserve-root'\
+	chown='chown --preserve-root'\
+	ga='git add'\
+	gaa='ga --all'\
+	gb='git branch'\
+	gs='git status'\
+	grep='grep --colour=auto --perl-regexp'\
+	rgrep='grep --dereference-recursive'\
+	zathura='zathura --plugins-dir=${XDG_DATA_HOME%/*}/lib'\
+	ls='ls --color=always'\
+	la='ls --almost-all --file-type'\
+	ll='la -l --sort=time --reverse --time-style=+"%H:%M %d/%m/%g"'\
+	ld='ll --directory'\
+	lr='ll --recursive'\
+ 
