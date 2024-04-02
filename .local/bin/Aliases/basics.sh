e0ec57c76f45a2210787596ac7eb91f6280b243c
KostisFioravantes <constfiora@gmail.com>
2024-04-03 01:02:19 +0300

    add alias dotfile for convenience

--- .local/bin/Aliases/basics.sh
+++ .local/bin/Aliases/basics.sh
@@ -3,10 +3,11 @@ alias\
 	chgrp='\chgrp --preserve-root'\
 	chmod='\chmod --preserve-root'\
 	chown='\chown --preserve-root'\
+	dotfile='git --git-dir="$HOME/dotfiles.git/" --work-tree="$HOME"'\
 	ga='\git add'\
 	gb='\git branch'\
 	gs='\git status'\
 	gaa='ga --all'\
 	grep='\grep --colour=auto --perl-regexp'\
 	rgrep='grep --dereference-recursive'\
-	rm='\rm -I'
+	rm='\rm -I'\
